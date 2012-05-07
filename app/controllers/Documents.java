82d9f212cea457486411088cf39e84915d7eecd6
Thilo Tanner <ttanner@astina.ch>
2012-05-07 13:22:50 +0200

    XML export for carrot2 added

--- app/controllers/Documents.java
+++ app/controllers/Documents.java
@@ -15,24 +15,33 @@ import play.Play;
 import play.mvc.Controller;
 import search.ElasticSearchPlugin;
 
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Documents extends Controller
 {
-    private static final int PAGE_SIZE = 10;
+    private static final int DEFAULT_PAGE_SIZE = 10;
 
     public static void index()
     {
         render();
     }
 
-    public static void search(String search, int page)
+    public static void search(String search, int page, int pageSize, String format) throws XMLStreamException
     {
         if(page < 1) {
             page = 1;
         }
+
+        if(pageSize == 0) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
 
         BoolQueryBuilder qb = QueryBuilders.boolQuery();
         if(search == null || search.isEmpty()) {
@@ -48,36 +57,62 @@ public class Documents extends Controller
         SearchResponse response = client.prepareSearch("documents")
                 .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
                 .setQuery(qb)
-                .setFrom((page - 1) * PAGE_SIZE).setSize(PAGE_SIZE)
+                .setFrom((page - 1) * pageSize).setSize(pageSize)
                 .execute()
                 .actionGet();
 
         renderArgs.put("search", search);
 
-        renderSearchResponse(response);
+        List<Document> documents = extractDocuments(response);
+
+        if("xml".equals(format)) {
+            renderXML(documents, search);
+        }
+
+        renderArgs.put("count", response.getHits().totalHits());
+        renderArgs.put("pageSize", pageSize);
+
+        render("@search", documents);
     }
 
-    public static void moreLikeThis(Long id, int page)
+    public static void moreLikeThis(Long id, int page, int pageSize, String format) throws XMLStreamException
     {
         if(page < 1) {
             page = 1;
         }
 
+        if(pageSize == 0) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+
         Client client = Play.plugin(ElasticSearchPlugin.class).client();
         MoreLikeThisRequest request = Requests.moreLikeThisRequest("documents");
-        request.searchFrom((page - 1) * PAGE_SIZE);
-        request.searchSize(PAGE_SIZE);
+        request.searchFrom((page - 1) * pageSize);
+        request.searchSize(pageSize);
         request.type("document");
         request.id(id.toString());
         SearchResponse response = client.moreLikeThis(request)
                 .actionGet();
 
-        renderArgs.put("thisDocument", Document.findById(id));
+        Document thisDocument = Document.findById(id);
 
-        renderSearchResponse(response);
+
+        List<Document> documents = extractDocuments(response);
+
+        if("xml".equals(format)) {
+            renderXML(documents, thisDocument.title);
+        }
+
+        renderArgs.put("thisDocument", thisDocument);
+
+        renderArgs.put("count", response.getHits().totalHits());
+        renderArgs.put("pageSize", pageSize);
+
+        render("@search", documents);
     }
 
-    private static void renderSearchResponse(SearchResponse response)
+    private static List<Document> extractDocuments(SearchResponse response)
     {
         DateTimeFormatter dateTimeFormatter = ISODateTimeFormat.dateTime();
 
@@ -97,9 +132,47 @@ public class Documents extends Controller
             documents.add(document);
         }
 
-        renderArgs.put("count", response.getHits().totalHits());
-        renderArgs.put("pageSize", PAGE_SIZE);
+        return documents;
+    }
 
-        render("@search", documents);
+    private static void renderXML(List<Document> documents, String query) throws XMLStreamException
+    {
+        StringWriter stringWriter = new StringWriter();
+        XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(stringWriter);
+
+        writer.writeStartDocument();
+
+        writer.writeStartElement("searchresult");
+
+        writer.writeStartElement("query");
+        writer.writeCharacters(query);
+        writer.writeEndElement(); // query
+
+        for(Document document : documents) {
+            writer.writeStartElement("document");
+            writer.writeAttribute("id", document.id.toString());
+
+            writer.writeStartElement("title");
+            writer.writeCharacters(document.title);
+            writer.writeEndElement(); // title
+
+            writer.writeStartElement("url");
+            writer.writeEndElement(); // title
+
+            writer.writeStartElement("snippet");
+            writer.writeCharacters(document.text);
+            writer.writeEndElement(); // query
+
+            writer.writeEndElement(); // document
+        }
+
+        writer.writeEndElement(); // searchresult
+
+        writer.writeEndDocument();
+
+        writer.flush();
+        writer.close();
+
+        renderXml(stringWriter.toString());
     }
 }
